ddf0cf7ba7f11260dc2d0f55c333228e4069ace6
christian <chmartin22@yahoo.es>
2021-03-09 15:12:33 +0100

    implementando ad/index.js

--- models/Ads/index.js
+++ models/Ads/index.js
@@ -1,4 +1,7 @@
 const Ad = require('./Ad');
 const adRoutes = require('./ad.routes');
+const adController = require('./ad.controller');
+const adFunctions = require('./ad.functions');
+const adHandler = require('./ad.handler');
 
-module.exports = { Ad, adRoutes };
+module.exports = { Ad, adRoutes, adController, adFunctions, adHandler };
